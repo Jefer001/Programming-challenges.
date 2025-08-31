d0f362857b442b263a58287f78b33f866ebfadc5
Jeferson Echavarria <jfrson24@gmail.com>
2025-08-30 19:12:15 -0500

    The Methods class is created.

--- Sln_Added_Up/Added_Up/Program.cs
+++ Sln_Added_Up/Added_Up/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ Write a program that finds the summation of every number from 1 to num (both inclusive). 
+ The number will always be a positive integer greater than 0. Your function only needs to return the resul.
+
+ Escribe un programa que calcule la suma de todos los números del 1 al num (ambos inclusive). 
+ El número siempre será un entero positivo mayor que 0. Tu función solo tiene que devolver el resultado
+ */
+
 Console.WriteLine("Hello, World!");
