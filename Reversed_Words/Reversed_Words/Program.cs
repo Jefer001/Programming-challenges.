6bdc2c66ed0d830765d68aed4d827376cea46f85
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-03 19:01:52 -0500

    The instance of the Methods class is created and the ReverseWords method is invoked.

--- Reversed_Words/Reversed_Words/Program.cs
+++ Reversed_Words/Reversed_Words/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*
+ Complete the solution so that it reverses all of the words within the string passed in.
+ Words are separated by exactly one space and there are no leading or trailing spaces.
+
+ Completa la solución para que invierta todas las palabras dentro de la cadena pasada.
+ Las palabras están separadas por un solo espacio y no hay espacios al principio ni al final.
+ */
+using Reversed_Words;
+
+Methods methods = new();
+
+Console.WriteLine("Enter the words you want to reverse.");
+string str = Console.ReadLine() ?? string.Empty;
+Console.WriteLine(methods.ReverseWords(str));
