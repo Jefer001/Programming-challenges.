039bf3844ff0d5d4eb14fca839d413f1c3d9e056
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-04 19:05:07 -0500

    The ScoreWords method is developed.

--- Sln_Highest_Scoring_Word/Highest_Scoring_Word/Methods.cs
+++ Sln_Highest_Scoring_Word/Highest_Scoring_Word/Methods.cs
@@ -1,13 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Highest_Scoring_Word
+﻿namespace Highest_Scoring_Word
 {
     public class Methods
     {
+        #region Public Methods
+        public static string ScoreWords(string str)
+        {
+            string[] words = str.Split(' ');
+            int maxPoints = 0;
+            string highestScoringWord = "";
+            foreach (string word in words)
+            {
+                int points = word.Sum(c => c - 'a' + 1);
+                if (points > maxPoints)
+                {
+                    maxPoints = points;
+                    highestScoringWord = word;
+                }
+            }
+            return highestScoringWord;
+        }
 
+        //public static string HighrestScoringWord(string str)
+        //{
+        //    string[] words = str.Split(' ');
+        //    int maxPoints = 0;
+        //    string highestScoringWord = "";
+        //    foreach (string word in words)
+        //    {
+        //        int points = 0;
+        //        foreach (char c in word)
+        //        {
+        //            points += c - 'a' + 1;
+        //        }
+        //        if (points > maxPoints)
+        //        {
+        //            maxPoints = points;
+        //            highestScoringWord = word;
+        //        }
+        //    }
+        //    return highestScoringWord;
+        //}
+        #endregion
     }
 }
--- Sln_Highest_Scoring_Word/Highest_Scoring_Word/Program.cs
+++ Sln_Highest_Scoring_Word/Highest_Scoring_Word/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*
+ Given a string of words, you need to find the highest scoring word.
+ Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+ For example, the score of abad is 8 (1 + 2 + 1 + 4).
+ You need to return the highest scoring word as a string.
+ If two words score the same, return the word that appears earliest in the original string.
+ All letters will be lowercase and all inputs will be valid.
+ */
+
+using Highest_Scoring_Word;
+
+//Methods methods = new Methods();
+Console.WriteLine("Enter the words to find out which one has the highest score.");
+string str = Console.ReadLine() ?? "";
+Console.WriteLine(Methods.ScoreWords(str.ToLower()));
