5fdcad2460a9492f029f2375af513f4e0d3ee944
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-09 19:52:35 -0500

    The isPrime2 method is developed.

--- Sln_NumberPrime/NumberPrime/Methods.cs
+++ Sln_NumberPrime/NumberPrime/Methods.cs
@@ -6,13 +6,25 @@ namespace NumberPrime
         #region Public Methods
         public static bool IsPrime(int number)
         {
-            if (number <= 1) return false;
+            if (number < 2) return false;
             if (number == 2) return true;
             if (number % 2 == 0) return false;
             for (int j = 3; j < number; j++)
             {
                 if (number % j == 0) return false;
             }
+            return true;
+        }
+
+        public bool isPrime2(int number)
+        {
+            if (number < 2) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+            int boundary = (int)System.Math.Floor(System.Math.Sqrt(number));
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
             return true;
         }
         #endregion
