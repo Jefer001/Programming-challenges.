4404f964b26d319d61dfb34044ac296141ef8abb
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-23 16:55:25 -0500

    New project created String_Ends_With

--- Sln_NumberPrime/NumberPrime/Methods.cs
+++ Sln_NumberPrime/NumberPrime/Methods.cs
@@ -21,7 +21,7 @@ namespace NumberPrime
             if (number < 2) return false;
             if (number == 2) return true;
             if (number % 2 == 0) return false;
-            int boundary = (int)System.Math.Floor(System.Math.Sqrt(number));
+            int boundary = (int)Math.Floor(Math.Sqrt(number));
             for (int i = 3; i <= boundary; i += 2)
                 if (number % i == 0)
                     return false;
