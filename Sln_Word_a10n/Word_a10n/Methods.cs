b4e68e807d27833189c925c4e4498d2a37d0e1c9
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-23 17:56:01 -0500

    The Abbreviate method is developed.

--- Sln_Word_a10n/Word_a10n/Methods.cs
+++ Sln_Word_a10n/Word_a10n/Methods.cs
@@ -3,6 +3,48 @@ namespace Word_a10n
 {
     public class Methods
     {
-
+        #region Public Methods
+        public string Abbreviate(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                return str;
+            var result = new System.Text.StringBuilder();
+            var word = new System.Text.StringBuilder();
+            foreach (char c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    word.Append(c);
+                }
+                else
+                {
+                    if (word.Length >= 4)
+                    {
+                        result.Append(word[0]);
+                        result.Append(word.Length - 2);
+                        result.Append(word[^1]);
+                    }
+                    else
+                    {
+                        result.Append(word);
+                    }
+                    result.Append(c);
+                    word.Clear();
+                }
+            }
+            // Handle the last word if there is one
+            if (word.Length >= 4)
+            {
+                result.Append(word[0]);
+                result.Append(word.Length - 2);
+                result.Append(word[^1]);
+            }
+            else
+            {
+                result.Append(word);
+            }
+            return result.ToString();
+        }
+        #endregion
     }
 }
