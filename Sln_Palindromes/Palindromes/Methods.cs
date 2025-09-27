3d9b59cb2dc8941a1b17c325600d1e95d11ccb34
Jeferson Echavarria <jfrson24@gmail.com>
2025-09-27 17:56:03 -0500

    The IsPalindrome method is developed.

--- Sln_Palindromes/Palindromes/Methods.cs
+++ Sln_Palindromes/Palindromes/Methods.cs
@@ -3,6 +3,20 @@ namespace Palindromes
 {
     public class Methods
     {
-
+        #region Private Methods
+        public static bool IsPalindrome(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            int left = 0;
+            int right = str.Length - 1;
+            while (left < right)
+            {
+                if (str[left] != str[right]) return false;
+                left++;
+                right--;
+            }
+            return true;
+        }
+        #endregion
     }
 }
--- Sln_Palindromes/Palindromes/Program.cs
+++ Sln_Palindromes/Palindromes/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+/*
+ You will be given a string and you task is to check if it is possible to convert that string into a palindrome by removing a single character. 
+ If the string is already a palindrome, return "OK". If it is not, and we can convert it to a palindrome by removing one character, then return "remove one", 
+ otherwise return "not possible". The order of the characters should not be changed.
+ */
+
+
+using Palindromes;
+
+Console.WriteLine("Enter a word to check if it is a palindrome");
+string str = Console.ReadLine() ?? "";
+
+if (Methods.IsPalindrome(str)) Console.WriteLine("If it is a palindrome.");
+else Console.WriteLine("It is not a palindrome.");
