b5e58dac532a6165c121dc22649c94fb811cd160
Jeferson Echavarria <jfrson24@gmail.com>
2025-10-05 10:30:16 -0500

    The Methods class is created.

---
+++ Sln_Maximum_Subarray_Sum/Maximum_Subarray_Sum/Methods.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Maximum_Subarray_Sum
+{
+    public class Methods
+    {
+    }
+}
--- Sln_Maximum_Subarray_Sum/Maximum_Subarray_Sum/Program.cs
+++ Sln_Maximum_Subarray_Sum/Maximum_Subarray_Sum/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers.
+ Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. 
+ If the list is made up of only negative numbers, return 0 instead. Your solution should be fast, it will be tested on very large arrays so slow solutions will time out.
+
+ Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.
+ */
+
 Console.WriteLine("Hello, World!");
