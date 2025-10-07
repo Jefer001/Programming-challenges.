96453f99b65421eaa3ae320bd30a5511f09a7042
Jeferson Echavarria <jfrson24@gmail.com>
2025-10-07 18:33:45 -0500

    The Sum_Strings_As_Numbers project is created.

---
+++ Sln_Sum_Strings_as_Numbers/Sum_Strings_as_Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ Given the string representations of two integers, return the string representation of the sum of those integers.
+ A string representation of an integer will contain no characters besides the ten numerals "0" to "9".
+
+ I have removed the use of BigInteger and BigDecimal in java
+ */
+
+Console.WriteLine("Hello, World!");
